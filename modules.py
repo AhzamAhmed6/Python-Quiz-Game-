d468a85d7408d75870d50cf72923ab39b672d833
Ahzam <ahzamahmed6@gmail.com>
2021-12-07 00:49:42 -0800

    init commit

--- modules.py
+++ modules.py
@@ -81,26 +81,33 @@ def quiz():
 	while True:
 		wd_list = list(word_dict) #list of words
 		choice_list = []#choicelist of definitions
+
 		for x in range(4):#max 4 choices
 			word, definition = get_def_and_pop(wd_list, word_dict)
 			choice_list.append(definition)
+
 		#shuffle the choices
 		random.shuffle(choice_list)
 		#printing a word as a quiz question
 		print(word)
 		print(f"{color.BOLD}-------------{color.END}")
 		#printing the choices using for each loop
+
 		for idx, choice in enumerate(choice_list):
 			#printing index
 			print(idx+1, choice)
+
 		#getting input from the user
 		choice = int(input(f"{color.YELLOW}{color.BOLD}Enter 1,2,3,4; 0 to exit: {color.END}"))
+		
 		#if user selects the correct choice
 		if choice_list[choice-1] == definition:
 			print(f"{color.GREEN}{color.BOLD}Correct!{color.END}\n")
+
 		#if user wants to exit
 		elif choice == 0:
 			exit(0)
+
 		#if user selects the wrong choice
 		else:
 			print(f"{color.RED}{color.BOLD}Incorrect You lose this{color.END}\n")
